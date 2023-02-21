17648d7f2390a5282c898427dfb79fb1efbab36f
Lilou Alidor <lilou.alidor@gmail.com>
2023-02-21 14:42:48 +0100

    Boids floating around

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,50 @@
 #define DOCTEST_CONFIG_IMPLEMENT
 #include "doctest/doctest.h"
 
+struct Boid {
+private:
+    float m_posX;
+    float m_posY;
+    int   m_angle;
+    float m_speed;
+    float m_color;
+
+public:
+    Boid()
+        : m_posX(0), m_posY(0), m_angle(0), m_speed(0), m_color(1.f) {}
+    Boid(float posX, float posY, int angle, float speed, float color)
+        : m_posX(posX), m_posY(posY), m_angle(angle), m_speed(speed), m_color(color) {}
+    void setPos(float posX, float posY)
+    {
+        m_posX = posX;
+        m_posY = posY;
+    }
+    void setAngle(int angle)
+    {
+        m_angle = angle;
+    }
+    float getX() const { return m_posX; }
+    float getY() const { return m_posY; }
+    int   getAngle() const { return m_angle; }
+    float getSpeed() const { return m_speed; }
+    float getColor() const { return m_color; }
+};
+
+float Deg2Rad(int deg)
+{
+    return static_cast<float>(deg * (M_PI / static_cast<float>(180)));
+}
+
+glm::vec2 newPosFromAngle(float centerX, float centerY, int angle, float speed)
+{
+    glm::vec2 newPos;
+
+    newPos.x = (speed * cos(Deg2Rad(angle))) / 2.f + centerX;
+    newPos.y = (speed * sin(Deg2Rad(angle))) / 2.f + centerY;
+
+    return newPos;
+}
+
 int main(int argc, char* argv[])
 {
     { // Run the tests
@@ -21,16 +65,12 @@ int main(int argc, char* argv[])
 
     float screenWidth = ctx.aspect_ratio();
 
-    std::vector<std::vector<float>> randSquare;
+    std::vector<Boid> randSquare;
 
-    for (int i = 0; i < 100; i++)
+    for (int i = 0; i < 50; i++)
     {
-        randSquare.emplace_back();
         glm::vec2 RandCoord = p6::random::point();
-        randSquare[i].push_back(RandCoord.x * ctx.aspect_ratio());
-        randSquare[i].push_back(RandCoord.y);
-        randSquare[i].push_back(floorf(p6::random::number(4)));
-        randSquare[i].push_back(p6::random::number());
+        randSquare.emplace_back(RandCoord.x * screenWidth, RandCoord.y, p6::random::integer(1, 360), 0.002, p6::random::number(0.25f, 1.f));
     }
 
     // Declare your infinite update loop.
@@ -42,38 +82,33 @@ int main(int argc, char* argv[])
 
         for (auto& i : randSquare)
         {
-            ctx.fill = {i[3], 0.f, 0.f};
+            ctx.fill = {i.getColor(), 0.f, 0.f};
             ctx.square(
-                p6::Center{i[0], i[1]},
-                p6::Radius{0.1f}
+                p6::Center{i.getX(), i.getY()},
+                p6::Radius{0.03f}
             );
 
-            switch (static_cast<int>(i[2]))
+            int direction = p6::random::integer(1, 100);
+
+            if (direction <= 15)
             {
-            case 0:
-                i[1] += 0.01f;
-                if (i[1] > 1.f)
-                    i[2] = 2;
-                break;
-
-            case 1:
-                i[0] += 0.01f;
-                if (i[0] > screenWidth)
-                    i[2] = 4;
-                break;
-
-            case 2:
-                i[1] -= 0.01f;
-                if (i[1] < -1.f)
-                    i[2] = 0;
-                break;
-
-            default:
-                i[0] -= 0.01f;
-                if (i[0] < -screenWidth)
-                    i[2] = 1;
-                break;
+                i.setAngle(i.getAngle() + 11);
             }
+            else if (direction >= 86)
+            {
+                i.setAngle(i.getAngle() - 11);
+            }
+
+            glm::vec2 newPos = newPosFromAngle(i.getX(), i.getY(), i.getAngle(), i.getSpeed());
+            if (newPos.x > screenWidth)
+                newPos.x = -1.f * screenWidth;
+            if (newPos.y > 1)
+                newPos.y = -1.f;
+            if (newPos.x < -screenWidth)
+                newPos.x = 1.f * screenWidth;
+            if (newPos.y < -1)
+                newPos.y = 1.f;
+            i.setPos(newPos.x, newPos.y);
         }
     };
 
